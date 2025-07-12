21406f68c2bb42943396eefb9c28cfec9637f956
altommo <41879727+altommo@users.noreply.github.com>
2025-07-12 04:05:55 +0100

    Add page modules and integrate

---
+++ src/js/pages/performers.js
@@ -0,0 +1,14 @@
+/**
+ * Performers Page Module
+ */
+export class PerformersPage {
+    constructor(core) {
+        this.core = core;
+    }
+
+    init() {
+        console.log('PerformersPage: initialized');
+    }
+}
+
+export default PerformersPage;
---
+++ src/js/pages/video-single.js
@@ -0,0 +1,14 @@
+/**
+ * Video Single Page Module
+ */
+export class VideoSinglePage {
+    constructor(core) {
+        this.core = core;
+    }
+
+    init() {
+        console.log('VideoSinglePage: initialized');
+    }
+}
+
+export default VideoSinglePage;
